c00db14224563b98e1b6dbae3864f79797c36738
AndresCarignano <103561195+AndresCarignano@users.noreply.github.com>
2022-04-13 17:34:07 -0300

    Update Data exploration Antenas de wifi.sql

--- Data exploration Antenas de wifi.sql
+++ Data exploration Antenas de wifi.sql
@@ -1,67 +1,112 @@
+
+//Analisis del listado de los puntos de acceso a internet de la Red WiFi de Pa√≠s Digital 
+//que brinda acceso gratuito a internet en espacios y dependencias p√∫blicas en Argentina
+//Fuente de descarga del dataset: https://www.datos.gob.ar/dataset/jgm-puntos-wifi-pais-digital
+
 USE [PuntosWifi]
 GO
+
+
 
 -- Cantidad de antenas y cantidad de municipios cubiertos por provincia
 
 SELECT  provincia
-		,COUNT(id) cant_antenas
-		,COUNT (DISTINCT municipio) 'municipios cubiertos'
+	,COUNT(id) cant_antenas
+	,COUNT (DISTINCT municipio) 'municipios cubiertos'
   FROM [conectiwidad-wifi]
   GROUP BY provincia
   ORDER BY cant_antenas DESC
+  
+  
+  
 
--- Provincia, Municipio, ubicacion e identificidador de antena
+-- Listado de Provincia, Municipio, ubicacion e identificidador de antena
 
-SELECT provincia, municipio,ubicacion,identificador
+SELECT  provincia
+	,municipio
+	,ubicacion
+	,identificador
 FROM [conectiwidad-wifi]
 ORDER BY provincia
 
--- Antenas en Plazas y parques
-SELECT provincia, municipio,ubicacion plazas_parques,identificador
+
+
+
+
+-- Antenas en plazas y parques
+
+SELECT  provincia
+	,municipio
+	,ubicacion plazas_parques
+	,identificador
 FROM [conectiwidad-wifi]
-WHERE ubicacion LIKE '%plaza%' OR ubicacion LIKE '%pza%' OR ubicacion LIKE '%parque%'
+WHERE ubicacion LIKE '%plaza%' 
+OR ubicacion LIKE '%pza%' 
+OR ubicacion LIKE '%parque%'
 ORDER BY provincia
 
---Antenas en Puntos digitales
-SELECT provincia, municipio,ubicacion puntos_digitales,identificador
+
+
+
+
+-- Antenas en Puntos digitales
+SELECT  provincia
+	,municipio
+	,ubicacion puntos_digitales
+	,identificador
 FROM [conectiwidad-wifi]
 WHERE ubicacion LIKE '%PD%'
 OR ubicacion LIKE '%punto%'
 ORDER BY provincia
 
--- cant de antenas por municipio y porcentaje de la provincia
-SELECT 
-	  provincia
-	  ,municipio
-	  ,COUNT(id) antenas_por_municipio
-	  ,CONVERT(decimal(5,2),COUNT(id)) * 100 / CONVERT(decimal(5,2),SUM(COUNT(id))
+
+
+
+
+-- Cantidad de antenas por municipio y porcentaje del total provincial
+SELECT  provincia
+	,municipio
+	,COUNT(id) antenas_por_municipio
+	,CONVERT(decimal(5,2),COUNT(id)) * 100 / CONVERT(decimal(5,2),SUM(COUNT(id))
 		OVER (Partition by provincia)) 'Porcentaje de la provincia'
 
   FROM [dbo].[conectiwidad-wifi]
   GROUP BY provincia,municipio
   ORDER BY provincia, antenas_por_municipio DESC
 
+
+
+
+
+
 -- Latitud y longitud de cada antena
 
-SELECT identificador, latitud,longitud
+SELECT  identificador
+	,latitud
+	,longitud
 FROM [conectiwidad-wifi]
 WHERE latitud != 0
 AND longitud != 0
 ORDER BY identificador
 
--- Antenas que no poseen ubicaciÛn geogr·fica
 
-SELECT identificador, latitud,longitud
+
+
+
+-- Listado de antenas con latitud y longitud indefinida
+
+SELECT  identificador
+	,latitud,longitud
 FROM [conectiwidad-wifi]
 WHERE latitud = 0
 AND longitud = 0
 ORDER BY identificador
 
-  -- Cuenta de antenas por provincia
-SELECT 
-	  provincia
-	  ,municipio
-	  ,SUM(COUNT(DISTINCT identificador)) OVER (Partition by provincia order by municipio) 'suma_cant_antenas'
+-- Cuenta de antenas por provincia
+SELECT  provincia
+	,municipio
+	,SUM(COUNT(DISTINCT identificador)) 
+		OVER (Partition by provincia order by municipio) 'suma_cant_antenas'
 
   FROM [dbo].[conectiwidad-wifi]
   GROUP BY provincia,municipio
